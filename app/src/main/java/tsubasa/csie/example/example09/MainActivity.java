38caee551672f6be710b642229c30f7080e1f2c0
Koyama-Tsubasa <a0918050152@gmail.com>
2019-05-07 13:08:30 +0800

    ch

--- app/src/main/java/tsubasa/csie/example/example09/MainActivity.java
+++ app/src/main/java/tsubasa/csie/example/example09/MainActivity.java
@@ -1,10 +1,14 @@
 package tsubasa.csie.example.example09;
 
 import android.app.Activity;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.content.Intent;
+import android.widget.TextView;
+
+import java.util.Random;
 
 public class MainActivity extends Activity {
 
@@ -25,4 +29,17 @@ public class MainActivity extends Activity {
         });
 
     }
+
+    public void changecolor (View v) {
+
+        Random x = new Random();
+        int r = x.nextInt(256);
+        int b = x.nextInt(256);
+        int g = x.nextInt(256);
+
+        TextView t = findViewById(R.id.textView);
+        t.setTextColor(Color.rgb(r,g,b));
+
+    }
+
 }
--- app/src/main/java/tsubasa/csie/example/example09/SecondActivity.java
+++ app/src/main/java/tsubasa/csie/example/example09/SecondActivity.java
@@ -1,18 +1,27 @@
 package tsubasa.csie.example.example09;
 
 import android.app.Activity;
+import android.graphics.Color;
 import android.os.Bundle;
+import android.support.constraint.ConstraintLayout;
+import android.text.SpannableStringBuilder;
+import android.text.style.ForegroundColorSpan;
 import android.view.View;
 import android.widget.Button;
 import android.content.Intent;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
+
+import java.util.Random;
 
 public class SecondActivity extends Activity {
-
+    ConstraintLayout t;
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activitylayout2);
-
+        t = findViewById(R.id.content);
         Button b2 = (Button)findViewById(R.id.button2);
         b2.setOnClickListener(new Button.OnClickListener()
         {
@@ -23,6 +32,33 @@ public class SecondActivity extends Activity {
                 SecondActivity.this.finish();
             }
         });
+
+    }
+
+    public void changebackgroundcolor (View v) {
+
+        Random x = new Random();
+        int r = x.nextInt(256);
+        int b = x.nextInt(256);
+        int g = x.nextInt(256);
+
+        t.setBackgroundColor(Color.rgb(r,g,b));
+
+    }
+
+    public void changetextcolor2 (View v) {
+
+        Random x = new Random();
+        int r = x.nextInt(256);
+        int b = x.nextInt(256);
+        int g = x.nextInt(256);
+
+        TextView t = findViewById(R.id.textView2);
+
+        SpannableStringBuilder s = new SpannableStringBuilder(getString(R.string.act2));
+        s.setSpan(new ForegroundColorSpan(Color.rgb(r,g,b)),8,17,0);
+
+        t.setText(s);
 
     }
 
